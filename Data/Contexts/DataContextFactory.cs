7a257cb71a25344ef54ba523b837138e7cb3a759
felyc <fely.coman@gmail.com>
2024-05-19 12:46:26 +0200

    subscribe/unsubscribe done

---
+++ Data/Contexts/DataContextFactory.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Data.Contexts;
+
+public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+{
+    public DataContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+        optionsBuilder.UseSqlServer("Server=tcp:sqlserver-silicon-project.database.windows.net,1433;Initial Catalog=blazorwebapp;Persist Security Info=False;User ID=SqlAdminSilicon;Password=Felici@89;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+        return new DataContext(optionsBuilder.Options);
+    }
+}
